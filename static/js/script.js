4a61487766808cb432f03ae37b61b7f5858a899c
susmithagudapati <susmitha.gudapati@gmail.com>
2017-05-25 20:24:56 +0530

    feat: Add locations in sidebar

--- static/js/script.js
+++ static/js/script.js
@@ -2,26 +2,31 @@ var locations = [
     {
         title: 'IIT Bombay',
         position: {lat: 19.1334302, lng: 72.91326789999999},
+        visible: ko.observable(true),
         id: 1
     },
     {
         title: 'Gateway of India',
         position: {lat: 18.9219841, lng: 72.8346543},
+        visible: ko.observable(true),
         id: 2
     },
     {
         title: 'Mount Mary Basilica',
         position: {lat: 19.046508, lng: 72.82243810000001},
+        visible: ko.observable(true),
         id: 3
     },
     {
         title: 'Haji Ali Dargah',
         position: {lat: 18.982747, lng:72.8089648},
+        visible: ko.observable(true),
         id: 4
     },
     {
         title: 'Chhatrapati Shivaji International Airport (BOM)',
         position: {lat: 19.0895595, lng: 72.8656144},
+        visible: ko.observable(true),
         id: 5
     }
 ];
@@ -51,20 +56,32 @@ function initialize() {
                         id: id
                     });
 
-        google.maps.event.addListener(this.marker, 'click', function() {
+        this.infowindow = google.maps.event.addListener(this.marker, 'click', function() {
             largeInfowindow.setContent('<div>' + this.title + this.position + '</div>');
             largeInfowindow.open(map, this);
         });
         bounds.extend(this.marker.position)
     };
 
-    var AppViewModel = function() {
-        locations.forEach(function(location) {
-            var marker = new Marker(location);
-            markers.push(marker);
-        });
-        map.fitBounds(bounds);
+    locations.forEach(function(location) {
+        var marker = new Marker(location);
+        markers.push(marker);
+    });
+
+    map.fitBounds(bounds);
+
+    var AppViewModel = {
+        markers: markers,
+        query: ko.observable(''),
     };
 
-    ko.applyBindings(new AppViewModel());
+    AppViewModel.search = ko.dependentObservable(function() {
+        var self = this;
+        var search = this.query().toLowerCase();
+        return ko.utils.arrayFilter(markers, function(marker) {
+            return marker.title.toLowerCase().indexOf(search) >= 0;
+        });
+    }, AppViewModel);
+
+    ko.applyBindings(AppViewModel);
 }
